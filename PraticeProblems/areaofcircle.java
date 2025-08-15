b2e18898541597ca18edc21415c47e88c1972aa3
Mitali <mitali8116@gmail.com>
2025-08-15 17:30:55 +0530

    update with codes

--- PraticeProblems/areaofcircle.java
+++ PraticeProblems/areaofcircle.java
@@ -1,5 +1,12 @@
-public class areaofcircle {
-public static void main(String[] args) {
-System.out.println("Hello, World!");
-}
+import java.util.Scanner;
+
+public class AreaofCircle {
+        public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the radius: ");
+        int radius = scanner.nextInt();
+        int area = (int) (Math.PI * radius * radius);
+        System.out.println("Area of the circle: " + area);
+    }
+
 }
