d0a68ba0e10646def031f352ddfadfae4a00dd4b
Mitali <mitali8116@gmail.com>
2026-03-31 15:51:38 +0530

    changes

---
+++ Queue/disticntpair.java
@@ -0,0 +1,19 @@
+
+
+public class disticntpair
+ {
+    public static void main(String[] args) {
+        int[] arr = {5,1,3};
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                int diff = Math.abs(arr[i] - arr[j]);
+                set.add(diff);
+            }
+        }
+
+        System.out.println("Distinct:" + set);
+        System.out.println(set.size());
+    }
+}
